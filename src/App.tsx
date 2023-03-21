2dbe50403b474b09fab2517a3da851efb1256339
Matayay Karuna <matayay2@gmail.com>
2023-03-21 03:01:16 -0600

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./stylesheets/App.css";
 import History from "./components/History";
 import Discovery from "./components/Discovery";
@@ -8,6 +8,7 @@ import { Photo } from "./components/Photo";
 
 function App() {
     const [photo_array, setArray] = useState<Photo[]>([]);
+    const [fetch, setFetch] = useState(true);
 
     const [clicks, setClicks] = useState(0);
     const handleClicks = () => {
@@ -21,13 +22,15 @@ function App() {
                 "https://api.thecatapi.com/v1/images/search?order=RAND&has_breeds=1&api_key=live_BCotpnlCcvjeC7WEza4FwLN7jhW4QnqySiKgFUgv9ejyAxB9d7xoswbs9bY9IbdI"
             );
             setPhoto(response.data[0]);
+            setFetch(true);
         };
 
-        if (clicks > 0) {
+        if (clicks > 0 && fetch) {
             fetchData();
             if (photo) {
                 photo_array.push(photo);
                 setArray(photo_array);
+                setFetch(false);
             }
         }
     }, [clicks]);
